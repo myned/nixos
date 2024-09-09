802f3c677cd484fb82f2314d43f71ca43e346909
Myned <dev@bjork.tech>
2024-09-09 17:24:21 -0500

    laptop: enable building while offline
    Signed-off-by: Myned <dev@bjork.tech>

--- profiles/laptop/default.nix
+++ profiles/laptop/default.nix
@@ -4,4 +4,8 @@
     profile = "desktop"; # Inherit desktop
     services.auto-cpufreq.enable = true;
   };
+
+  #!! Rebuild offline - drastically increases initial download and resulting closure size
+  # https://discourse.nixos.org/t/rebuild-nixos-offline/3679
+  system.includeBuildDependencies = true;
 }
