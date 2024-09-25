7665a9a4ccd1d98bc74408819a2c7d921b078d7e
Myned <dev@bjork.tech>
2024-09-25 15:14:23 -0500

    ssh: fix fhs permissions
    Signed-off-by: Myned <dev@bjork.tech>

--- options/custom/programs/ssh.nix
+++ options/custom/programs/ssh.nix
@@ -15,13 +15,22 @@ in {
       StrictHostKeyChecking no
     '';
 
-    home-manager.users.${config.custom.username}.programs.ssh = {
-      enable = true;
+    home-manager.users.${config.custom.username} = {
+      programs.ssh = {
+        enable = true;
 
-      extraConfig = ''
-        Host *
-          IdentityAgent ~/.1password/agent.sock
-      '';
+        extraConfig = ''
+          Host *
+            IdentityAgent ~/.1password/agent.sock
+        '';
+      };
+
+      # Work around FHS permissions
+      # https://github.com/nix-community/home-manager/issues/322#issuecomment-1856128020
+      home.file.".ssh/config" = {
+        target = ".ssh/config_source";
+        onChange = "cat ~/.ssh/config_source > ~/.ssh/config && chmod 400 ~/.ssh/config";
+      };
     };
   };
 }
