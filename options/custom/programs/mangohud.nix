ae00cbe12ea0964ef75153895a096c48cdf06140
Myned <dev@bjork.tech>
2024-10-13 23:08:07 -0500

    mangohud: enable adaptive vsync and sort
    Signed-off-by: Myned <dev@bjork.tech>

--- options/custom/programs/mangohud.nix
+++ options/custom/programs/mangohud.nix
@@ -16,28 +16,30 @@ in {
       enableSessionWide = true;
 
       settings = {
-        core_load = true;
-        frame_timing = true;
-        gpu_stats = true;
-        no_display = true; # Hide by default
-        ram = true;
-        vram = true;
         background_alpha = 0;
-        font_size = 24;
-        fps_limit = "100,75,60"; # !! Monitor dependent
         background_color = "002b36";
+        core_load = true;
         cpu_color = "268bd2";
         device_battery = "gamepad,mouse";
         engine_color = "dc322f";
         font_file = "${pkgs.nerdfonts}/share/fonts/truetype/NerdFonts/IosevkaNerdFont-Bold.ttf";
+        font_size = 24;
+        fps_limit = "100,75,60"; # !! Monitor dependent
         fps_limit_method = "early"; # Smoother frametimes compared to late
+        frame_timing = true;
         frametime_color = "859900";
+        gl_vsync = -1; # Adaptive
         gpu_color = "2aa198";
+        gpu_stats = true;
+        no_display = true; # Hide by default
         position = "top-center";
+        ram = true;
         ram_color = "d33682";
         text_color = "ffffff";
         toggle_fps_limit = "Control_L+period";
         toggle_hud = "Control_L+slash";
+        vsync = 0; # Adaptive
+        vram = true;
         vram_color = "6c71c4";
       };
     };
