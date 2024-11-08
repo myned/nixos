f8a247495583ff223dd0cd7378034427eac0704f
Myned <dev@bjork.tech>
2024-11-08 09:39:38 -0600

    slurp: fix sway build
    Signed-off-by: Myned <dev@bjork.tech>

--- options/custom/programs/slurp.nix
+++ options/custom/programs/slurp.nix
@@ -12,11 +12,11 @@ in {
     # https://github.com/emersion/slurp
     # https://github.com/emersion/slurp/blob/master/slurp.1.scd
     home.sessionVariables.SLURP_ARGS = lib.concatStringsSep " " [
-      "-B 00000000"
+      "'-B 00000000"
       "-b 00000000"
       "-c d33682"
       #// "-s d3368240"
-      "-w 2"
+      "-w 2'"
     ];
   };
 }
