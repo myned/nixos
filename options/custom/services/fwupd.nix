eab362ef0b4d4e1cdb45a5c2f4aefcdf6b228d56
Myned <dev@bjork.tech>
2024-11-25 12:00:57 -0600

    fwupd: add bug comment
    Signed-off-by: Myned <dev@bjork.tech>

--- options/custom/services/fwupd.nix
+++ options/custom/services/fwupd.nix
@@ -9,6 +9,9 @@ in {
   options.custom.services.fwupd.enable = mkOption {default = false;};
 
   config = mkIf cfg.enable {
+    # BUG: Cache not refreshing properly with libxmlb < 0.3.21
+    # https://github.com/fwupd/fwupd/issues/7918
+    # https://github.com/NixOS/nixpkgs/issues/347248
     # https://wiki.nixos.org/wiki/Fwupd
     # https://github.com/fwupd/fwupd
     services.fwupd = {
