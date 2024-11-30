bbceefdb0e6a0d4e7d16e298d1852bad23eecb18
Myned <dev@bjork.tech>
2024-11-30 16:01:38 -0600

    anime-game-launcher: simplify options
    Signed-off-by: Myned <dev@bjork.tech>

--- options/custom/programs/anime-game-launcher.nix
+++ options/custom/programs/anime-game-launcher.nix
@@ -6,13 +6,28 @@
 with lib; let
   cfg = config.custom.programs.anime-game-launcher;
 in {
-  options.custom.programs.anime-game-launcher.enable = mkOption {default = false;};
+  options.custom.programs.anime-game-launcher = {
+    enable = mkOption {default = false;};
+    genshin-impact = mkOption {default = false;};
+    honkai-impact = mkOption {default = false;};
+    honkai-star-rail = mkOption {default = false;};
+    universal = mkOption {default = false;};
+    wuthering-waves = mkOption {default = false;};
+    zenless-zone-zero = mkOption {default = false;};
+  };
 
   config = mkIf cfg.enable {
     # https://github.com/an-anime-team
     # https://github.com/ezKEa/aagl-gtk-on-nix
-    #?? If error on first setup, clone components
+    #!! If error on first setup, clone components
     #?? git clone https://github.com/an-anime-team/components.git
-    programs.honkers-railway-launcher.enable = true;
+    programs = {
+      anime-game-launcher.enable = cfg.genshin-impact;
+      anime-games-launcher.enable = cfg.universal;
+      honkers-railway-launcher.enable = cfg.honkai-star-rail;
+      honkers-launcher.enable = cfg.honkai-impact;
+      sleepy-launcher.enable = cfg.zenless-zone-zero;
+      wavey-launcher.enable = cfg.wuthering-waves;
+    };
   };
 }
