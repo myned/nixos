5bdf7fc7cbb3f797985652133ef434ec08687aa2
Myned <dev@bjork.tech>
2024-11-30 16:02:21 -0600

    desktop: enable anime-game-launcher
    Signed-off-by: Myned <dev@bjork.tech>

--- profiles/desktop/default.nix
+++ profiles/desktop/default.nix
@@ -4,5 +4,10 @@
     profile = "desktop";
     wallpaper = true;
     services.power-profiles-daemon.enable = true;
+
+    programs.anime-game-launcher = {
+      enable = true;
+      genshin-impact = true;
+    };
   };
 }
