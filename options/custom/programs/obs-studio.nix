6cf076d67f9dc0059342f26414e09561a94bd2a6
Myned <dev@bjork.tech>
2024-12-14 22:26:33 -0600

    obs-studio: add synced config
    Signed-off-by: Myned <dev@bjork.tech>

--- options/custom/programs/obs-studio.nix
+++ options/custom/programs/obs-studio.nix
@@ -11,5 +11,7 @@ in {
   config.home-manager.users.${config.custom.username} = mkIf cfg.enable {
     # https://github.com/obsproject/obs-studio
     programs.obs-studio.enable = true;
+
+    xdg.configFile."obs-studio".source = config.home-manager.users.${config.custom.username}.lib.file.mkOutOfStoreSymlink "${config.custom.sync}/common/config/obs-studio";
   };
 }
