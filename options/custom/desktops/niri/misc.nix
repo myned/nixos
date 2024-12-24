aa6fac79c69807325b36833437aad6568cdc5187
Myned <dev@bjork.tech>
2024-12-23 22:54:53 -0500

    niri: add sway-audio-idle-inhibit
    Signed-off-by: Myned <dev@bjork.tech>

--- options/custom/desktops/niri/misc.nix
+++ options/custom/desktops/niri/misc.nix
@@ -1,10 +1,12 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
 with lib; let
   niri = "${config.programs.niri.package}/bin/niri";
+  sway-audio-idle-inhibit = "${pkgs.sway-audio-idle-inhibit}/bin/sway-audio-idle-inhibit";
 
   cfg = config.custom.desktops.niri.misc;
 in {
@@ -16,6 +18,10 @@ in {
     home-manager.users.${config.custom.username} = {
       programs.niri.settings = {
         # https://github.com/YaLTeR/niri/wiki/Configuration:-Miscellaneous
+
+        spawn-at-startup = [
+          {command = [sway-audio-idle-inhibit];} # Inhibit while audio is playing
+        ];
 
         # HACK: Inherit home-manager environment variables in lieu of upstream fix
         # https://github.com/nix-community/home-manager/issues/2659
