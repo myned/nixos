026a76aeec3b23bd785ea91e6e0c4783f24f4bec
Myned <dev@bjork.tech>
2024-12-23 22:54:09 -0500

    desktops: disable hyprland
    Signed-off-by: Myned <dev@bjork.tech>

--- options/custom/desktops/default.nix
+++ options/custom/desktops/default.nix
@@ -6,7 +6,7 @@
 with lib; {
   config.custom.desktops = mkIf config.custom.full {
     #// gnome.enable = true;
-    hyprland.enable = true;
+    #// hyprland.enable = true;
     #// kde.enable = true;
     niri.enable = true;
     #// sway.enable = true;
