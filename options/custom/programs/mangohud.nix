c1e923ef3cae5343674e21cdf1c4d3ec88b4dd03
Myned <dev@bjork.tech>
2025-01-02 22:13:11 -0600

    mangohud: fix font file
    Signed-off-by: Myned <dev@bjork.tech>

--- options/custom/programs/mangohud.nix
+++ options/custom/programs/mangohud.nix
@@ -26,7 +26,7 @@ in {
         device_battery = "gamepad,mouse";
         dynamic_frame_timing = true;
         engine_color = "dc322f";
-        font_file = "${pkgs.nerd-fonts.iosevka}/share/fonts/truetype/NerdFonts/IosevkaTermSlabNerdFontPropo-Medium.ttf";
+        font_file = "${pkgs.nerd-fonts.iosevka}/share/fonts/truetype/NerdFonts/Iosevka/IosevkaNerdFontPropo-SemiBold.ttf";
         font_size = 24;
         fps_limit = "100,75,60,50"; # !! Monitor dependent
         fps_limit_method = "early"; # Smoother frametimes compared to late
