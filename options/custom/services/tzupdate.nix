2efc835c270dcc3a85d6642bfd9a7e9177d664ee
Myned <dev@bjork.tech>
2025-01-09 20:25:26 -0600

    tzupdate: add fixme
    Signed-off-by: Myned <dev@bjork.tech>

--- options/custom/services/tzupdate.nix
+++ options/custom/services/tzupdate.nix
@@ -14,6 +14,7 @@ in {
 
     # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/nixos/modules/services/misc/tzupdate.nix
     systemd.services.tzupdate = {
+      # FIXME: Fails at boot, possibly needs to wait for tailscaled.service
       wantedBy = ["network-online.target"]; # Run at boot without checking for Internet access
     };
   };
