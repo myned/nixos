ea32eaf554506790b3088131d22a4be339b083e4
Myned <dev@bjork.tech>
2025-01-09 20:25:38 -0600

    ghostty: add config
    Signed-off-by: Myned <dev@bjork.tech>

--- options/custom/programs/ghostty.nix
+++ options/custom/programs/ghostty.nix
@@ -5,7 +5,6 @@
 }:
 with lib; let
   cfg = config.custom.programs.ghostty;
-  hm = config.home-manager.users.${config.custom.username};
 in {
   options.custom.programs.ghostty = {
     enable = mkOption {default = false;};
@@ -19,7 +18,52 @@ in {
           enable = true;
 
           # https://ghostty.org/docs/config/reference
-          settings = {
+          settings = rec {
+            adjust-cell-width = -1;
+
+            # https://ghostty.org/docs/config/reference#adw-toolbar-style
+            adw-toolbar-style = "flat";
+
+            # https://ghostty.org/docs/config/reference#cursor-style-blink
+            cursor-style-blink = false;
+
+            # https://ghostty.org/docs/config/reference#font-family
+            #?? ghostty +list-fonts
+            #?? ghostty +show-face
+            font-family = with config.custom.settings.fonts; [monospace fallback];
+            font-family-bold = font-family;
+
+            # https://ghostty.org/docs/config/reference#font-size
+            font-size = 14;
+
+            # https://ghostty.org/docs/config/reference#gtk-titlebar
+            gtk-titlebar = false;
+
+            # https://ghostty.org/docs/config/reference#mouse-hide-while-typing
+            mouse-hide-while-typing = true;
+
+            # https://ghostty.org/docs/config/reference#shell-integration-features
+            shell-integration-features = "no-cursor";
+
+            # https://ghostty.org/docs/features/theme
+            # https://ghostty.org/docs/config/reference#theme
+            #?? ghostty +list-themes
+            theme = "Builtin Solarized Dark";
+
+            # https://ghostty.org/docs/config/reference#window-decoration
+            window-decoration = false;
+
+            # https://ghostty.org/docs/config/reference#window-new-tab-position
+            window-new-tab-position = "end";
+
+            # https://ghostty.org/docs/config/reference#window-padding-balance
+            # BUG: Not always distributed equally
+            # https://github.com/ghostty-org/ghostty/discussions/3941
+            window-padding-balance = true;
+
+            # https://ghostty.org/docs/config/reference#window-padding-x
+            window-padding-x = 8;
+            window-padding-y = 4;
           };
         };
       }
