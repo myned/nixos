ef8f3957e80969ca85f67e84539f11a008c15744
Myned <dev@bjork.tech>
2025-01-11 11:53:06 -0600

    fonts: add typefaces and weight option
    Signed-off-by: Myned <dev@bjork.tech>

--- options/custom/settings/fonts.nix
+++ options/custom/settings/fonts.nix
@@ -9,11 +9,12 @@ with lib; let
 in {
   options.custom.settings.fonts = {
     enable = mkOption {default = false;};
-    emoji = mkOption {default = "Noto Color Emoji";};
+    emoji = mkOption {default = "Blobmoji";};
     fallback = mkOption {default = "Unifont";};
-    monospace = mkOption {default = "IosevkaTerm NFP SemiBold";};
-    sans-serif = mkOption {default = "Outfit";};
-    serif = mkOption {default = "Liberation Serif";};
+    monospace = mkOption {default = "IosevkaTermSlab NFP Medium";};
+    sans-serif = mkOption {default = "Afacad";};
+    serif = mkOption {default = "Lora";};
+    weight = mkOption {default = "500";};
   };
 
   config = mkIf cfg.enable {
@@ -31,25 +32,33 @@ in {
           iosevka-term
           iosevka-term-slab
           jetbrains-mono
+          shure-tech-mono
           space-mono
           zed-mono
         ]
         ++ (with pkgs; [
-          ### Sans Serif
           (google-fonts.override {
             fonts = [
+              ### Sans Serif
               # Text
-              "Jost"
-              "Lexend"
+              "Afacad"
               "Outfit"
+              "Urbanist"
 
               # Condensed
               "Oswald"
 
               # Pixel
+              "Geo"
               "Silkscreen"
+
+              ### Serif
+              "Lora"
             ];
           })
+
+          ### Emoji
+          noto-fonts-emoji-blob-bin
 
           ### Other
           # Microsoft
@@ -60,7 +69,7 @@ in {
           unifont
         ]);
 
-      #?? fc-list --brief | grep family: | sort
+      #?? fc-list : family | sort
       fontconfig.defaultFonts = {
         emoji = [cfg.emoji];
         monospace = [cfg.monospace];
