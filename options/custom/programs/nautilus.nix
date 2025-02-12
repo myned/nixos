77c7207d8bee889be6d46b7e32b9a79ea7b7ea77
Myned <dev@bjork.tech>
2025-02-12 12:31:50 -0600

    nautilus: disable service by default
    Signed-off-by: Myned <dev@bjork.tech>

--- options/custom/programs/nautilus.nix
+++ options/custom/programs/nautilus.nix
@@ -11,7 +11,7 @@ in {
     enable = mkOption {default = false;};
     git = mkOption {default = false;};
     index = mkOption {default = true;};
-    service = mkOption {default = true;};
+    service = mkOption {default = false;};
     terminal = mkOption {default = "ghostty";};
   };
 
@@ -44,6 +44,7 @@ in {
 
     home-manager.users.${config.custom.username} = {
       # HACK: Partially fix startup delay with background service until module is available
+      # BUG: --gapplication-service always exits without a window
       systemd.user.services = let
         nautilus = "${pkgs.nautilus}/bin/nautilus";
         turtle_service = "${pkgs.turtle}/bin/turtle_service";
@@ -56,7 +57,7 @@ in {
             BusName = "org.gnome.Nautilus";
             ExecStart = "${nautilus} --gapplication-service";
             ExecStop = "${nautilus} --quit";
-            Restart = "always"; #!! Benign exceptions cause nautilus to exit
+            Restart = "always"; #!! Exits when last window is closed
             Type = "dbus";
 
             # HACK: Allow child processes to live, otherwise applications launched through service are killed on stop
