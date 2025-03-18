08c278d2c9e7c723dd6ade70df61bb28291440b3
Myned <dev@bjork.tech>
2025-03-17 22:39:53 -0500

    nix: fix preemptively adding files to store

--- options/default.nix
+++ options/default.nix
@@ -1,5 +1,12 @@
 {lib, ...}:
 with lib; {
   # Import all *.nix options from custom directory, excluding .*.nix
-  imports = filter (f: hasSuffix ".nix" f && !hasPrefix "." (builtins.baseNameOf f)) (filesystem.listFilesRecursive ./custom);
+  imports =
+    filter (
+      file: let
+        f = builtins.baseNameOf file;
+      in
+        !hasPrefix "." f && hasSuffix ".nix" f
+    )
+    (filesystem.listFilesRecursive ./custom);
 }
