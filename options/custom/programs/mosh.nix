6483e240e147aff8cb1e3ec5b82e9905316219cf
Myned <dev@bjork.tech>
2025-03-26 02:51:09 -0500

    mosh: always enable prediction

--- options/custom/programs/mosh.nix
+++ options/custom/programs/mosh.nix
@@ -6,12 +6,18 @@
 with lib; let
   cfg = config.custom.programs.mosh;
 in {
-  options.custom.programs.mosh.enable = mkOption {default = false;};
+  options.custom.programs.mosh = {
+    enable = mkOption {default = false;};
+  };
 
   config = mkIf cfg.enable {
-    # https://wiki.nixos.org/wiki/Mosh
+    # https://mosh.org/
     # https://github.com/mobile-shell/mosh
+    # https://wiki.nixos.org/wiki/Mosh
     programs.mosh.enable = true; # !! Opens UDP ports 60000-61000
-    environment.shellAliases.mosh = "mosh --predict-overwrite";
+
+    environment.shellAliases = {
+      mosh = "mosh --predict=always --predict-overwrite";
+    };
   };
 }
