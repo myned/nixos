95827c16fe97a1f76f57bf9ac3419fc69ab23bae
Myned <dev@bjork.tech>
2025-04-02 09:37:32 -0500

    auto-cpufreq: increase epp

--- options/custom/services/auto-cpufreq.nix
+++ options/custom/services/auto-cpufreq.nix
@@ -25,9 +25,12 @@ in {
         enable = true;
 
         # https://github.com/AdnanHodzic/auto-cpufreq/blob/master/auto-cpufreq.conf-example
+        #?? cat /sys/devices/system/cpu/cpu0/cpufreq/energy_performance_available_preferences
+        #?? cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_governors
+        #?? cat /sys/firmware/acpi/platform_profile_choices
         settings = {
           battery = {
-            energy_performance_preference = "power";
+            energy_performance_preference = "balance_power";
             governor = "powersave";
             platform_profile = "low-power";
             scaling_max_freq = mkIf (isFloat cfg.max.battery || isInt cfg.max.battery) (builtins.floor (cfg.max.battery * 1000 * 1000)); # KHz
@@ -35,7 +38,7 @@ in {
           };
 
           charger = {
-            energy_performance_preference = "balance_power";
+            energy_performance_preference = "balance_performance";
             governor = "powersave";
             platform_profile = "balanced";
             scaling_max_freq = mkIf (isFloat cfg.max.charger || isInt cfg.max.charger) (builtins.floor (cfg.max.charger * 1000 * 1000)); # KHz
