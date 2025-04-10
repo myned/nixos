67d46ec7a10c606b048b2b2406f4684c6af17b24
Myned <dev@bjork.tech>
2025-04-09 21:30:12 -0500

    docker: enable userland proxy to fix container routing

--- options/custom/containers/default.nix
+++ options/custom/containers/default.nix
@@ -46,11 +46,12 @@ in {
 
         # https://docs.docker.com/reference/cli/dockerd/#daemon-configuration-file
         daemon.settings = {
-          # Disable userland-proxy to pass client IP to containers
+          # FIXME: Container cannot route via public address to another container on the same host
+          #!! Disable userland-proxy to pass client IP to containers
           # https://github.com/moby/moby/issues/15086
           # https://github.com/moby/moby/issues/14856
           # https://github.com/docker/docs/issues/17312
-          userland-proxy = false;
+          #// userland-proxy = false;
         };
       };
 
