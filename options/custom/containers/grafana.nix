563a8d8c6659a9f106bf1c4f2157ae6f2cfb1770
Myned <dev@bjork.tech>
2025-04-16 22:21:52 -0500

    containers: add grafana

---
+++ options/custom/containers/grafana.nix
@@ -0,0 +1,116 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib; let
+  cfg = config.custom.containers.grafana;
+in {
+  options.custom.containers.grafana = {
+    enable = mkEnableOption "grafana";
+
+    prometheus = {
+      enable = mkOption {
+        default = true;
+        type = types.bool;
+      };
+
+      # https://prometheus.io/docs/prometheus/latest/configuration/configuration/
+      settings = mkOption {
+        default = {
+          scrape_configs = [
+            {
+              # https://prometheus.io/docs/guides/node-exporter/#configuring-your-prometheus-instances
+              job_name = "node";
+              scrape_interval = "5s";
+              static_configs = [{targets = cfg.prometheus.targets;}];
+            }
+          ];
+        };
+
+        type = types.attrs;
+      };
+
+      targets = mkOption {
+        default = ["myne:9100" "mypi3:9100"];
+        type = with types; listOf str;
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    #?? arion-grafana pull
+    environment.shellAliases.arion-grafana = "sudo arion --prebuilt-file ${config.virtualisation.arion.projects.grafana.settings.out.dockerComposeYaml}";
+
+    virtualisation.arion.projects.grafana.settings = {
+      services =
+        {
+          # https://github.com/grafana/grafana
+          # https://grafana.com/
+          # https://grafana.com/docs/grafana/latest/setup-grafana/installation/docker/
+          grafana.service = {
+            container_name = "grafana";
+            image = "grafana/grafana-oss:11.6.0"; # https://hub.docker.com/r/grafana/grafana-oss/tags
+            ports = ["127.0.0.1:3100:3000/tcp"];
+            restart = "unless-stopped";
+            volumes = ["${config.custom.containers.directory}/grafana/data:/var/lib/grafana"];
+
+            # https://grafana.com/docs/grafana/latest/setup-grafana/configure-grafana/
+            environment = {
+              GF_PLUGINS_PREINSTALL_DISABLED = "true";
+            };
+          };
+        }
+        // optionalAttrs cfg.prometheus.enable {
+          # https://github.com/prometheus/prometheus
+          # https://prometheus.io/
+          # https://prometheus.io/docs/prometheus/latest/installation/
+          # https://grafana.com/docs/grafana/latest/datasources/prometheus/configure-prometheus-data-source/
+          prometheus.service = {
+            container_name = "grafana-prometheus";
+            dns = mkIf config.services.tailscale.enable ["100.100.100.100"]; # Tailscale resolver
+            image = "quay.io/prometheus/prometheus:v3.3.0"; # https://quay.io/repository/prometheus/prometheus?tab=tags
+            ports = ["${config.custom.services.tailscale.ip}:9090:9090/tcp"];
+            restart = "unless-stopped";
+
+            volumes = [
+              "${pkgs.writeText "prometheus.yml" (generators.toYAML {} cfg.prometheus.settings)}:/etc/prometheus/prometheus.yml"
+              "${config.custom.containers.directory}/grafana/prometheus/data:/prometheus"
+            ];
+          };
+        };
+    };
+
+    # https://github.com/grafana/grafana/blob/main/packaging/docker/custom/Dockerfile
+    #?? arion-grafana run -- --rm --entrypoint='id grafana' grafana
+    systemd.tmpfiles.settings.grafana = let
+      owner = mode: {
+        inherit mode;
+        user = "472"; # grafana
+        group = "472"; # grafana
+      };
+    in {
+      "${config.custom.containers.directory}/grafana/data" = {
+        d = owner "0700"; # -rwx------
+        z = owner "0700"; # -rwx------
+      };
+    };
+
+    # https://github.com/prometheus/prometheus/blob/main/Dockerfile
+    #?? arion-prometheus run -- --rm --entrypoint='id nobody' prometheus
+    systemd.tmpfiles.settings.prometheus = let
+      owner = mode: {
+        inherit mode;
+        user = "65534"; # nobody
+        group = "65534"; # nobody
+      };
+    in
+      mkIf cfg.prometheus.enable {
+        "${config.custom.containers.directory}/grafana/prometheus/data" = {
+          d = owner "0700"; # -rwx------
+          z = owner "0700"; # -rwx------
+        };
+      };
+  };
+}
