7c0dc719183273118c80b0d48c69a9c0fa82af6a
Myned <dev@bjork.tech>
2025-04-18 20:12:30 -0500

    myork: disable fixes

--- machines/myork/default.nix
+++ machines/myork/default.nix
@@ -44,41 +44,32 @@
     };
   };
 
-  services.keyd.keyboards.default.settings.main.rightcontrol = "layer(altgr)"; # No Ctrl_R
+  boot.kernelParams = [
+    # Disable AMD scaling driver
+    # https://wiki.archlinux.org/title/CPU_frequency_scaling#amd_pstate
+    #// "amd_pstate=disable"
 
-  boot = {
-    # Enable hibernation with a swapfile on btrfs
-    # https://wiki.archlinux.org/title/Power_management/Suspend_and_hibernate#Hibernation_into_swap_file
-    #?? findmnt -no UUID -T /swap/swapfile
-    resumeDevice = "/dev/disk/by-uuid/f9416347-eff5-45d5-8dc3-93414c11ba6f";
+    # Force display power savings
+    # https://wiki.archlinux.org/title/Framework_Laptop_13#(AMD)_Washed-out_colors_when_using_power-profiles-daemon_in_power-saver_or_balanced_mode
+    #// "amdgpu.abmlevel=0"
 
-    kernelParams = [
-      #?? sudo btrfs inspect-internal map-swapfile -r /swap/swapfile
-      "resume_offset=533760"
+    # Attempt to fix battery drain with suspend
+    # https://wiki.archlinux.org/title/Framework_Laptop_13#Suspend-then-hibernate_on_AMD_version
+    #// "rtc_cmos.use_acpi_alarm=1"
 
-      # Fix battery drain with suspend
-      # https://wiki.archlinux.org/title/Framework_Laptop_13#Suspend-then-hibernate_on_AMD_version
-      "rtc_cmos.use_acpi_alarm=1"
+    # Attempt to fix graphical slowdowns
+    # https://gitlab.freedesktop.org/drm/amd/-/issues/3647
+    #// "amdgpu.dcdebugmask=0x10"
 
-      # Force display power savings
-      # https://wiki.archlinux.org/title/Framework_Laptop_13#(AMD)_Washed-out_colors_when_using_power-profiles-daemon_in_power-saver_or_balanced_mode
-      #// "amdgpu.abmlevel=0"
+    # Attempt to fix display flickering
+    #// "amdgpu.sg_display=0"
 
-      # Disable AMD scaling driver
-      # https://wiki.archlinux.org/title/CPU_frequency_scaling#amd_pstate
-      #// "amd_pstate=disable"
-    ];
-  };
+    # Attempt to fix corruption when resuming from suspend
+    # https://github.com/NixOS/nixos-hardware/issues/1348
+    #// "pcie_aspm=off"
+  ];
 
-  home-manager.users.${config.custom.username} = {
-    wayland.windowManager.hyprland.settings = {
-      device = [
-        {
-          name = "pixa3854:00-093a:0274-touchpad";
-          accel_profile = "adaptive";
-          sensitivity = 0.3;
-        }
-      ];
-    };
+  services = {
+    keyd.keyboards.default.settings.main.rightcontrol = "layer(altgr)"; # Alt_R
   };
 }
