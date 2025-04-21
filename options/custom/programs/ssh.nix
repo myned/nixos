9b66071217763fa73f8967fddca4d45f83eac174
Myned <dev@bjork.tech>
2025-04-21 02:46:57 -0500

    ssh: use matchBlocks option

--- options/custom/programs/ssh.nix
+++ options/custom/programs/ssh.nix
@@ -6,27 +6,37 @@
 with lib; let
   cfg = config.custom.programs.ssh;
 in {
-  options.custom.programs.ssh.enable = mkOption {default = false;};
+  options.custom.programs.ssh = {
+    enable = mkOption {default = false;};
+  };
 
   config = mkIf cfg.enable {
-    home-manager.users.${config.custom.username} = {
-      programs.ssh = {
-        enable = true;
+    home-manager.sharedModules = [
+      {
+        programs.ssh = {
+          enable = true;
 
-        # https://ghostty.org/docs/help/terminfo#configure-ssh-to-fall-back-to-a-known-terminfo-entry
-        extraConfig = ''
-          Host *
-            SetEnv TERM=xterm-256color
-            StrictHostKeyChecking accept-new
-        '';
-      };
+          matchBlocks = {
+            "*" = {
+              setEnv = {
+                # https://ghostty.org/docs/help/terminfo#configure-ssh-to-fall-back-to-a-known-terminfo-entry
+                TERM = "xterm-256color";
+              };
 
-      # Work around FHS permissions
-      # https://github.com/nix-community/home-manager/issues/322#issuecomment-1856128020
-      # home.file.".ssh/config" = {
-      #   target = ".ssh/config_source";
-      #   onChange = "cat ~/.ssh/config_source > ~/.ssh/config && chmod 400 ~/.ssh/config";
-      # };
-    };
+              extraOptions = {
+                StrictHostKeyChecking = "accept-new";
+              };
+            };
+          };
+        };
+
+        # Work around FHS permissions
+        # https://github.com/nix-community/home-manager/issues/322#issuecomment-1856128020
+        # home.file.".ssh/config" = {
+        #   target = ".ssh/config_source";
+        #   onChange = "cat ~/.ssh/config_source > ~/.ssh/config && chmod 400 ~/.ssh/config";
+        # };
+      }
+    ];
   };
 }
