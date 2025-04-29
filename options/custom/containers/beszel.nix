b308a9b4f078223c875df53c6fa2938f88d13186
Myned <dev@bjork.tech>
2025-04-28 21:01:40 -0500

    beszel: guard agent behind option

--- options/custom/containers/beszel.nix
+++ options/custom/containers/beszel.nix
@@ -9,6 +9,11 @@ in {
   options.custom.containers.beszel = {
     enable = mkEnableOption "beszel";
 
+    agent = mkOption {
+      default = true;
+      type = types.bool;
+    };
+
     key = mkOption {
       default = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ4ESP/UERllv2j58bZMRhctAVjpikKIkrYsFWVSPmYe";
       type = types.str;
@@ -26,21 +31,16 @@ in {
 
     # https://beszel.dev/
     # https://github.com/henrygd/beszel
+    # https://beszel.dev/guide/hub-installation
     virtualisation.arion.projects.beszel.settings.services =
-      {
-        # https://beszel.dev/guide/agent-installation
-        agent.service = {
-          container_name = "beszel-agent";
-          image = "ghcr.io/henrygd/beszel/beszel-agent:latest"; # https://github.com/henrygd/beszel/pkgs/container/beszel%2Fbeszel-agent
-          network_mode = "host"; # 45876/tcp
+      optionalAttrs cfg.server {
+        beszel.service = {
+          container_name = "beszel";
+          depends_on = ["vpn"];
+          image = "ghcr.io/henrygd/beszel/beszel:latest"; # https://github.com/henrygd/beszel/pkgs/container/beszel%2Fbeszel
+          network_mode = "service:vpn"; # 8090/tcp
           restart = "unless-stopped";
-          volumes = ["/var/run/docker.sock:/var/run/docker.sock:ro"];
-
-          # https://beszel.dev/guide/environment-variables#agent
-          environment = {
-            KEY = cfg.key;
-            LISTEN = "${config.custom.services.tailscale.ip}:45876";
-          };
+          volumes = ["${config.custom.containers.directory}/beszel/data:/beszel_data"];
         };
 
         # https://tailscale.com/kb/1282/docker
@@ -58,15 +58,20 @@ in {
           };
         };
       }
-      // optionalAttrs cfg.server {
-        # https://beszel.dev/guide/hub-installation
-        beszel.service = {
-          container_name = "beszel";
-          depends_on = ["vpn"];
-          image = "ghcr.io/henrygd/beszel/beszel:latest"; # https://github.com/henrygd/beszel/pkgs/container/beszel%2Fbeszel
-          network_mode = "service:vpn"; # 8090/tcp
+      // optionalAttrs cfg.agent {
+        # https://beszel.dev/guide/agent-installation
+        agent.service = {
+          container_name = "beszel-agent";
+          image = "ghcr.io/henrygd/beszel/beszel-agent:latest"; # https://github.com/henrygd/beszel/pkgs/container/beszel%2Fbeszel-agent
+          network_mode = "host"; # 45876/tcp
           restart = "unless-stopped";
-          volumes = ["${config.custom.containers.directory}/beszel/data:/beszel_data"];
+          volumes = ["/var/run/docker.sock:/var/run/docker.sock:ro"];
+
+          # https://beszel.dev/guide/environment-variables#agent
+          environment = {
+            KEY = cfg.key;
+            LISTEN = "${config.custom.services.tailscale.ip}:45876";
+          };
         };
       };
   };
