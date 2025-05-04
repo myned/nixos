0c6e006ec1b65cab34eb4dd018649b384fc318f0
Myned <dev@bjork.tech>
2025-05-03 20:20:22 -0500

    firefox: revert software webrender

--- options/custom/programs/firefox/.common.nix
+++ options/custom/programs/firefox/.common.nix
@@ -199,7 +199,7 @@ with lib; {
         "general.autoScroll" = false;
         "general.smoothScroll" = true;
         "general.smoothScroll.msdPhysics.enabled" = false;
-        "gfx.webrender.software" = config.custom.settings.hardware.igpu;
+        "gfx.webrender.software" = false;
         "identity.fxaccounts.enabled" = true;
         "layers.acceleration.force-enabled" = true;
         "layout.css.always_underline_links" = false;
