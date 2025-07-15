a05fcf4cc1d27726528df1d4b41b3f6061b8e76b
Myned <dev@bjork.tech>
2025-07-14 21:58:53 -0500

    fingerprints.sh: only delete previous prints once

--- options/custom/scripts/fingerprints.sh
+++ options/custom/scripts/fingerprints.sh
@@ -3,9 +3,10 @@
 # Enroll fingerprints
 # https://wiki.archlinux.org/title/Fprint#Create_fingerprint_signature
 
+sudo fprintd-delete root
+sudo fprintd-delete "$USER"
+
 for finger in {left,right}-{thumb,{index,middle}-finger}; do
-  sudo fprintd-delete root
-  sudo fprintd-delete "$USER"
   sudo fprintd-enroll -f "$finger" "$USER"
   sleep 2
 done
