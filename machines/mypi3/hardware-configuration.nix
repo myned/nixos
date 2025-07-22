b061b96e5e0e645d217236d0aeba991991cb86a7
Myned <dev@bjork.tech>
2025-07-21 21:35:56 -0500

    mypi3: update hardware-configuration.nix

--- machines/mypi3/hardware-configuration.nix
+++ machines/mypi3/hardware-configuration.nix
@@ -8,19 +8,14 @@
   modulesPath,
   ...
 }: {
-  imports = [(modulesPath + "/installer/scan/not-detected.nix")];
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
 
   boot.initrd.availableKernelModules = [];
   boot.initrd.kernelModules = [];
   boot.kernelModules = [];
   boot.extraModulePackages = [];
-
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
-    fsType = "ext4";
-  };
-
-  swapDevices = [];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
