749e9583a884ed47e24db16ff659bc1c2e0da03e
Myned <dev@bjork.tech>
2025-07-27 17:06:37 -0500

    mynix: revert removal of amdgpu workaround

--- machines/mynix/default.nix
+++ machines/mynix/default.nix
@@ -122,6 +122,19 @@
     #// "amdgpu.dcdebugmask=0x10" # Attempt to fix random freezes by disabling panel self refresh
   ];
 
+  # Work around performance issues with amdgpu power scaling
+  # https://wiki.archlinux.org/title/AMDGPU#Screen_artifacts_and_frequency_problem
+  # https://wiki.archlinux.org/title/AMDGPU#Power_profiles
+  # 0=BOOTUP_DEFAULT 1=3D_FULL_SCREEN 2=POWER_SAVING 3=VIDEO 4=VR 5=COMPUTE 6=CUSTOM
+  #!! cardX or renderX must match the correct gpu
+  #?? lspci
+  #?? ls -l /dev/dri/by-path/*
+  #?? sudo udevadm trigger /dev/dri/by-path/*
+  #?? grep '*' /sys/class/drm/card*/device/pp_power_profile_mode
+  services.udev.extraRules = ''
+    KERNEL=="renderD129", SUBSYSTEM=="drm", DRIVERS=="amdgpu", ATTR{device/power_dpm_force_performance_level}="manual", ATTR{device/pp_power_profile_mode}="1"
+  '';
+
   #  _._     _,-'""`-._
   # (,-.`._,'(       |\`-/|
   #     `-.-' \ )-`( , o o)
