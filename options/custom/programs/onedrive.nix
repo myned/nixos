fa0eea4bfe56ce3946d7f501f202af6061dd3b5f
Myned <dev@bjork.tech>
2025-08-11 19:54:00 -0500

    onedrive: add workarounds

--- options/custom/programs/onedrive.nix
+++ options/custom/programs/onedrive.nix
@@ -6,29 +6,33 @@
 with lib; let
   cfg = config.custom.programs.onedrive;
 in {
-  options.custom.programs.onedrive.enable = mkOption {default = false;};
+  options.custom.programs.onedrive = {
+    enable = mkEnableOption "onedrive";
+  };
 
-  config.home-manager.users.${config.custom.username} = mkIf cfg.enable {
-    # https://github.com/abraunegg/onedrive
-    #!! Login is imperative
-    #?? onedrive
-    #?? systemctl --user enable --now onedrive.service
-    programs.onedrive = {
-      enable = true;
+  config = mkIf cfg.enable {
+    home-manager.users.${config.custom.username} = {
+      # https://github.com/abraunegg/onedrive
+      #!! Login is imperative
+      #?? onedrive
+      #?? systemctl --user enable --now onedrive.service
+      programs.onedrive = {
+        enable = true;
 
-      # https://github.com/abraunegg/onedrive/blob/master/docs/USAGE.md#configuration
-      settings = {
-        sync_dir = "~/OneDrive";
+        # https://github.com/abraunegg/onedrive/blob/master/docs/application-config-options.md
+        settings = {
+          #!! Attempt to work around Obsidian atomic state
+          delay_inotify_processing = "true";
+          force_session_upload = "true";
+
+          #!! SharePoint "file enrichment" modifies uploads
+          # https://github.com/OneDrive/onedrive-api-docs/issues/935
+          disable_upload_validation = "true";
+
+          local_first = "true"; # Use local files as source of truth
+          sync_dir = "~/OneDrive";
+        };
       };
     };
-
-    # xdg.configFile = {
-    #   # https://github.com/abraunegg/onedrive/blob/master/docs/USAGE.md#performing-a-selective-sync-via-sync_list-file
-    #   "onedrive/sync_list".text = ''
-    #     !/Apps/
-    #     !/Attachments/
-    #     /*
-    #   '';
-    # };
   };
 }
