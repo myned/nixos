cf0e8c2d3d49d70ca80a1a743a6e70efb925b3af
Myned <dev@bjork.tech>
2025-08-13 21:54:33 -0500

    geoclue2: use default provider

--- options/custom/services/geoclue2.nix
+++ options/custom/services/geoclue2.nix
@@ -6,29 +6,43 @@
 }:
 with lib; let
   cfg = config.custom.services.geoclue2;
+
+  where-am-i = "${config.services.geoclue2.package}/libexec/geoclue-2.0/demos/where-am-i";
 in {
   options.custom.services.geoclue2 = {
-    enable = mkOption {default = false;};
-    static = mkOption {default = true;};
+    enable = mkEnableOption "geoclue2";
+
+    apps = mkOption {
+      default = [];
+      description = "Desktop ids of user apps to allow access to geolocation without authorization";
+      example = ["firefox"];
+      type = with types; listOf str;
+    };
+
+    static = mkOption {
+      default = false;
+      description = "Whether to use a static source";
+      example = true;
+      type = types.bool;
+    };
   };
 
   config = mkIf cfg.enable {
-    age.secrets = let
-      secret = filename: {
-        file = "${inputs.self}/secrets/${filename}";
-        owner = "geoclue";
-        group = "geoclue";
-      };
-    in {
-      "common/geoclue2/geolocation" = mkIf cfg.static (secret "common/geoclue2/geolocation");
-    };
-
     # https://gitlab.freedesktop.org/geoclue/geoclue
-    # FIXME: geoclue2 relies on MLS, which is retired
-    # https://github.com/NixOS/nixpkgs/issues/321121
+    # https://man.archlinux.org/man/extra/geoclue/geoclue.5.en
     services.geoclue2 =
       {
         enable = true;
+
+        # https://man.archlinux.org/man/extra/geoclue/geoclue.5.en#APPLICATION_CONFIGURATION_OPTIONS
+        appConfig = listToAttrs (forEach cfg.apps (name: {
+          inherit name;
+
+          value = {
+            isAllowed = true;
+            isSystem = false;
+          };
+        }));
       }
       // optionalAttrs cfg.static {
         # TODO: Use static source option when merged into unstable
@@ -41,15 +55,35 @@ in {
         enableWifi = false;
       };
 
-    # Manually use static source from coordinates
-    # https://github.com/NixOS/nixpkgs/issues/311595#issuecomment-2247989491
-    environment.etc = mkIf cfg.static {
-      "geolocation".source = config.age.secrets."common/geoclue2/geolocation".path;
+    environment = {
+      # Manually use static source from coordinates
+      # https://github.com/NixOS/nixpkgs/issues/311595#issuecomment-2247989491
+      etc = mkIf cfg.static {
+        "geolocation".source = config.age.secrets."common/geoclue2/geolocation".path;
 
-      "geoclue/conf.d/00-config.conf".text = ''
-        [static-source]
-        enable=true
-      '';
+        "geoclue/conf.d/00-config.conf".text = ''
+          [static-source]
+          enable=true
+        '';
+      };
+
+      # FIXME: Desktop file may be required for permissions
+      # HACK: Command not part of package outputs
+      #?? where-am-i
+      shellAliases = {
+        inherit where-am-i;
+      };
     };
+
+    age.secrets = let
+      secret = filename: {
+        file = "${inputs.self}/secrets/${filename}";
+        owner = "geoclue";
+        group = "geoclue";
+      };
+    in
+      mkIf cfg.static {
+        "common/geoclue2/geolocation" = secret "common/geoclue2/geolocation";
+      };
   };
 }
