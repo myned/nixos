586753252b62a8079750737e7cf48691b464cec3
Myned <dev@bjork.tech>
2025-08-13 19:46:01 -0500

    fprintd: disable fingerprint pam auth by default

--- options/custom/services/fprintd.nix
+++ options/custom/services/fprintd.nix
@@ -9,10 +9,16 @@ in {
   options.custom.services.fprintd.enable = mkOption {default = false;};
 
   config = mkIf cfg.enable {
-    # https://wiki.archlinux.org/title/Fprint
     # https://www.freedesktop.org/wiki/Software/fprint
+    # https://wiki.nixos.org/wiki/Fingerprint_scanner
+    # https://wiki.archlinux.org/title/Fprint
     #!! Configuration is imperative
     #?? fprintd-enroll -f FINGER
     services.fprintd.enable = true;
+
+    # TODO: Use alternate PAM module when packaged
+    # https://github.com/NixOS/nixpkgs/issues/395807
+    # https://wiki.nixos.org/wiki/Fingerprint_scanner#Login
+    security.pam.services.login.fprintAuth = false;
   };
 }
