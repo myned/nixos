0d555d3d0972874a7232788278209747e8150c33
Myned <dev@bjork.tech>
2025-08-13 21:55:17 -0500

    gammastep: allow geoclue2 access

--- options/custom/services/gammastep.nix
+++ options/custom/services/gammastep.nix
@@ -6,24 +6,32 @@
 with lib; let
   cfg = config.custom.services.gammastep;
 in {
-  options.custom.services.gammastep.enable = mkOption {default = false;};
+  options.custom.services.gammastep = {
+    enable = mkEnableOption "gammastep";
+  };
 
-  config.home-manager.users.${config.custom.username} = mkIf cfg.enable {
-    # https://gitlab.com/chinstrap/gammastep
-    services.gammastep = {
-      enable = true;
-      enableVerboseLogging = true;
-      provider = "geoclue2"; # geoclue2 service must be enabled
+  config = mkIf cfg.enable {
+    custom.services.geoclue2.apps = ["gammastep"];
 
-      temperature = {
-        day = 5000;
-        night = 4000;
-      };
+    home-manager.users.${config.custom.username} = {
+      # https://gitlab.com/chinstrap/gammastep
+      services.gammastep = {
+        enable = true;
+        enableVerboseLogging = true;
+        #// dawnTime = "06:00-08:00";
+        #// duskTime = "18:00-20:00";
+        provider = mkIf config.services.geoclue2.enable "geoclue2";
 
-      # TODO: Add keybinds
-      settings.general = {
-        brightness-day = 1;
-        brightness-night = 0.6;
+        temperature = {
+          day = 5000;
+          night = 4000;
+        };
+
+        # TODO: Add keybinds
+        settings.general = {
+          brightness-day = 1;
+          brightness-night = 0.6;
+        };
       };
     };
   };
