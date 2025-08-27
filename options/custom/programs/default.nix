d0e434b3028f0c96c6e25d3ca1126325a16f851a
Myned <dev@bjork.tech>
2025-08-26 19:26:44 -0500

    custom: switch to firefox

--- options/custom/programs/default.nix
+++ options/custom/programs/default.nix
@@ -42,7 +42,7 @@ with lib; {
       nautilus.enable = true;
       nvtop.enable = true;
       #// wezterm.enable = true;
-      zen-browser.enable = true;
+      #// zen-browser.enable = true;
     })
 
     (mkIf config.custom.full {
