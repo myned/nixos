111812100b25e025f05c559a4087a4978926a54b
Myned <dev@bjork.tech>
2025-09-12 18:25:21 -0500

    niri: reduce struts

--- options/custom/desktops/niri/layout.nix
+++ options/custom/desktops/niri/layout.nix
@@ -84,8 +84,8 @@ in {
         struts = let
           strut = gap;
         in {
-          left = strut * 2;
-          right = strut * 2;
+          left = strut;
+          right = strut;
           top = strut;
           bottom = strut;
         };
