99922a8ac00e9a642f4c642f34c5a251e7354516
Myned <dev@bjork.tech>
2025-09-13 19:31:33 -0500

    swaync: update custom theme

--- options/custom/services/swaync/default.nix
+++ options/custom/services/swaync/default.nix
@@ -15,36 +15,37 @@ in {
 
       # FIXME: Fix GTK4 theme or use stylix
       # https://github.com/ErikReider/SwayNotificationCenter/blob/main/data/style/style.scss
-      # style = let
-      #   border = toString config.custom.border;
-      #   gap = toString config.custom.gap;
-      # in ''
-      #   .control-center {
-      #     border: ${border} solid #073642;
-      #     margin: ${gap}px;
-      #   }
+      #?? GTK_DEBUG=interactive swaync
+      style = let
+        border = toString config.custom.border;
+        gap = toString config.custom.gap;
+      in ''
+        ${readFile ./style.css}
 
-      #   .notification.low {
-      #     border: ${border} solid #6c71c4;
-      #   }
+        .control-center {
+          border: ${border}px solid #073642;
+          margin: ${gap}px;
+        }
 
-      #   .notification.normal {
-      #     border: ${border} solid #d33682;
-      #   }
+        .notification.low {
+          border: ${border}px solid #6c71c4;
+        }
 
-      #   .notification.critical {
-      #     border: ${border} solid #dc322f;
-      #   }
+        .notification.normal {
+          border: ${border}px solid #d33682;
+        }
 
-      #   ${readFile ./style.css}
-      # '';
+        .notification.critical {
+          border: ${border}px solid #dc322f;
+        }
+      '';
 
       # https://github.com/ErikReider/SwayNotificationCenter/blob/main/src/configSchema.json
       settings = {
         control-center-height = builtins.floor (config.custom.height * 0.5); # 50%
         control-center-positionY = "bottom";
-        control-center-width = 1000;
-        fit-to-screen = false;
+        control-center-width = 750;
+        fit-to-screen = true;
         hide-on-clear = true;
         notification-2fa-action = false;
         notification-icon-size = 32;
@@ -58,6 +59,9 @@ in {
 
         widgets = [
           "notifications"
+          "mpris"
+          "volume"
+          "backlight"
           "inhibitors"
           "dnd"
           "title"
@@ -66,6 +70,6 @@ in {
     };
 
     # https://stylix.danth.me/options/modules/swaync.html
-    stylix.targets.swaync.enable = true;
+    stylix.targets.swaync.enable = false;
   };
 }
