8f86dba848d86c87c87204fc764c486c0619fc74
Myned <dev@bjork.tech>
2025-09-22 20:24:28 -0500

    games: fix symlinks with whitespace in path

--- options/custom/games/fist.nix
+++ options/custom/games/fist.nix
@@ -14,7 +14,7 @@ in {
     # https://www.protondb.com/app/1330470
     # https://www.pcgamingwiki.com/wiki/F.I.S.T.:_Forged_in_Shadow_Torch
     systemd.user.tmpfiles.rules = [
-      "L+ ${config.custom.games.heroicDir}/F.I.S.T. Forged In Shadow Torch/drive_c/users/${config.custom.username}/AppData/Local/ZingangGame/Saved/Config/SaveGames - - - - ${config.custom.sync}/game/saves/fist/${config.custom.username}/SaveGames"
+      "L+ '${config.custom.games.heroicDir}/F.I.S.T. Forged In Shadow Torch/drive_c/users/${config.custom.username}/AppData/Local/ZingangGame/Saved/Config/SaveGames' - - - - ${config.custom.sync}/game/saves/fist/${config.custom.username}/SaveGames"
     ];
   };
 }
