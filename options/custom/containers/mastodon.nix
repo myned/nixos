5ae8db26e0956bd6d798cafcdd7ff326c5b47a5d
Myned <dev@bjork.tech>
2025-10-06 02:08:36 -0500

    mastodon: switch to glitch-soc fork

--- options/custom/containers/mastodon.nix
+++ options/custom/containers/mastodon.nix
@@ -7,29 +7,23 @@
 with lib; let
   cfg = config.custom.containers.mastodon;
 in {
-  options.custom.containers.mastodon.enable = mkOption {default = false;};
+  options.custom.containers.mastodon = {
+    enable = mkEnableOption "mastodon";
+  };
 
   config = mkIf cfg.enable {
-    age.secrets = let
-      secret = filename: {
-        file = "${inputs.self}/secrets/${filename}";
-      };
-    in {
-      "${config.custom.hostname}/mastodon/.env" = secret "${config.custom.hostname}/mastodon/.env";
-      "${config.custom.hostname}/mastodon/db.env" = secret "${config.custom.hostname}/mastodon/db.env";
-    };
-
     #?? arion-mastodon pull
     environment.shellAliases.arion-mastodon = "sudo arion --prebuilt-file ${config.virtualisation.arion.projects.mastodon.settings.out.dockerComposeYaml}";
 
     virtualisation.arion.projects.mastodon.settings.services = {
+      # https://github.com/glitch-soc/mastodon
       # https://github.com/linuxserver/docker-mastodon
       # https://github.com/mastodon/mastodon/blob/main/docker-compose.yml
       mastodon.service = {
         container_name = "mastodon";
         depends_on = ["cache" "db"];
         env_file = [config.age.secrets."${config.custom.hostname}/mastodon/.env".path];
-        image = "ghcr.io/linuxserver/mastodon:4.4.2"; # https://github.com/linuxserver/docker-mastodon/pkgs/container/mastodon
+        image = "ghcr.io/linuxserver/mastodon:4.4.5-glitch"; # https://github.com/linuxserver/docker-mastodon/pkgs/container/mastodon
         ports = ["3000:443/tcp"];
         restart = "unless-stopped";
         volumes = ["${config.custom.containers.directory}/mastodon/config:/config"];
@@ -50,5 +44,14 @@ in {
         volumes = ["${config.custom.containers.directory}/mastodon/db:/var/lib/postgresql/data"];
       };
     };
+
+    age.secrets = listToAttrs (map (name: {
+        inherit name;
+        value = {file = "${inputs.self}/secrets/${name}";};
+      })
+      [
+        "${config.custom.hostname}/mastodon/.env"
+        "${config.custom.hostname}/mastodon/db.env"
+      ]);
   };
 }
