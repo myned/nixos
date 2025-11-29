19e94bd49a1d9793b3da0491ee154f150d0298c3
Myned <dev@bjork.tech>
2025-11-28 20:46:53 -0600

    steam: enable extest
    Signed-off-by: Myned <dev@bjork.tech>

--- options/custom/programs/steam.nix
+++ options/custom/programs/steam.nix
@@ -10,36 +10,41 @@ in {
   # https://wiki.nixos.org/wiki/Steam
   # https://store.steampowered.com
   options.custom.programs.steam = {
-    enable = mkOption {default = false;};
-    extest = mkOption {default = false;};
+    enable = mkEnableOption "steam";
+
+    extest = mkOption {
+      default = true;
+      description = "Whether to enable Wayland cursor compatibility hack";
+      example = false;
+      type = types.bool;
+    };
   };
 
   config = mkIf cfg.enable {
-    programs.steam =
-      {
-        enable = true;
-        extest.enable = cfg.extest; # Work around invisible cursor on Wayland
-        extraCompatPackages = [pkgs.proton-ge-bin];
-        localNetworkGameTransfers.openFirewall = true; # 27040/tcp 27036/udp
-        remotePlay.openFirewall = true; # 27036/tcp/udp 27031-27035/udp
+    programs.steam = {
+      enable = true;
+      protontricks.enable = true;
+      extest.enable = cfg.extest;
+      extraCompatPackages = [pkgs.proton-ge-bin];
+      localNetworkGameTransfers.openFirewall = true; # 27040/tcp 27036/udp
+      remotePlay.openFirewall = true; # 27036/tcp/udp 27031-27035/udp
 
-        # HACK: Work around black main window with xwayland-satellite
-        # https://github.com/ValveSoftware/steam-for-linux/issues/10543 et al.
-        package = (
-          pkgs.steam.override {
-            extraArgs = "-system-composer";
+      # HACK: Work around black main window with xwayland-satellite
+      # https://github.com/ValveSoftware/steam-for-linux/issues/10543 et al.
+      package = (
+        pkgs.steam.override {
+          extraArgs = "-system-composer";
 
-            extraEnv = {
-              # HACK: Force XInput controller so only Steam Input is used, requires Proton GE
-              # https://github.com/Heroic-Games-Launcher/HeroicGamesLauncher/issues/4168
-              # https://github.com/GloriousEggroll/proton-ge-custom/wiki/Changelog#ge-proton10-7-hotfix
-              PROTON_DISABLE_HIDRAW = 1;
-              PROTON_PREFER_SDL = 1;
-            };
-          }
-        );
-      }
-      // optionalAttrs (versionAtLeast version "24.11") {protontricks.enable = true;};
+          extraEnv = {
+            # HACK: Force XInput controller so only Steam Input is used, requires Proton GE
+            # https://github.com/Heroic-Games-Launcher/HeroicGamesLauncher/issues/4168
+            # https://github.com/GloriousEggroll/proton-ge-custom/wiki/Changelog#ge-proton10-7-hotfix
+            PROTON_DISABLE_HIDRAW = 1;
+            PROTON_PREFER_SDL = 1;
+          };
+        }
+      );
+    };
 
     environment.systemPackages = with pkgs; [
       adwsteamgtk # https://github.com/Foldex/AdwSteamGtk
