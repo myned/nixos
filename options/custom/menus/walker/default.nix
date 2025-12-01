baddb1d0dbf4f1c9ad14679f00ab24546e3d8de6
Myned <dev@bjork.tech>
2025-11-30 21:49:49 -0600

    walker: fix show commands
    Signed-off-by: Myned <dev@bjork.tech>

--- options/custom/menus/walker/default.nix
+++ options/custom/menus/walker/default.nix
@@ -23,16 +23,17 @@ in {
       # TODO: Update commands
       menus = mapAttrsRecursive (path: value: toString (pkgs.writeShellScript (concatStringsSep "-" (["menus"] ++ path)) value)) {
         default.show = walker;
+        calculator.show = "${walker} --provider=calc";
 
         clipboard = {
-          show = "${walker} --modules clipboard";
-          clear = "${rm} ~/.cache/walker/clipboard.gob && ${notify-send} '> walker' 'Clipboard cleared' --urgency low";
-          clear-silent = "${rm} ~/.cache/walker/clipboard.gob";
+          show = "${walker} --provider=clipboard";
+          clear = "${rm} ~/.cache/elephant/clipboard.gob && ${notify-send} '> walker' 'Clipboard cleared' --urgency low";
+          clear-silent = "${rm} ~/.cache/elephant/clipboard.gob";
         };
 
-        dmenu.show = "${walker} --modules dmenu";
-        emoji.show = "${walker} --modules emojis";
-        search.show = "${walker} --modules search";
+        dmenu.show = "${walker} --dmenu";
+        emoji.show = "${walker} --provider=symbols";
+        search.show = "${walker} --provider=websearch";
         vault.show = "";
       };
 
@@ -62,220 +63,9 @@ in {
             // {
               click_to_close = false;
               close_when_open = true;
+              hide_quick_activation = true;
               force_keyboard_focus = true;
-              resume_last_query = true;
-
-              # list = {
-              #   placeholder = "";
-              # };
-
-              # search = {
-              #   placeholder = "";
-              #   #// resume_last_query = true;
-              # };
-
-              # # https://github.com/abenz1267/walker/wiki/Modules
-              # # https://github.com/PapirusDevelopmentTeam/papirus-icon-theme/tree/master/Papirus/64x64
-              # disabled = [
-              #   "ai"
-              #   "commands"
-              #   "custom_commands"
-              #   "finder"
-              #   "websearch" # Replaced by custom plugin
-              #   "windows"
-              # ];
-
-              # builtins = {
-              #   applications = {
-              #     actions.enabled = false;
-              #     hide_without_query = true;
-              #     placeholder = "";
-              #     show_generic = false;
-              #     switcher_only = false;
-              #   };
-
-              #   bookmarks = {
-              #     icon = "user-bookmarks";
-              #     placeholder = "";
-              #     prefix = "b ";
-              #     switcher_only = false;
-              #   };
-
-              #   calc = {
-              #     icon = "accessories-calculator";
-              #     min_chars = 1;
-              #     placeholder = "";
-              #     prefix = "=";
-              #     show_icon_when_single = true;
-              #     switcher_only = false;
-              #   };
-
-              #   clipboard = {
-              #     max_entries = 50;
-              #     placeholder = "";
-              #     switcher_only = true;
-              #   };
-
-              #   dmenu = {
-              #     keep_sort = true;
-              #     placeholder = "Input";
-              #     switcher_only = true;
-              #   };
-
-              #   emojis = {
-              #     placeholder = "";
-              #     prefix = "`";
-              #     switcher_only = false;
-              #   };
-
-              #   finder = {
-              #     icon = "filetypes";
-              #     placeholder = "";
-              #     prefix = "//";
-              #     show_icon_when_single = true;
-              #     switcher_only = false;
-              #   };
-
-              #   runner = {
-              #     icon = "utilities-x-terminal";
-              #     placeholder = "";
-              #     prefix = ">";
-              #     show_icon_when_single = true;
-              #     switcher_only = false;
-              #   };
-
-              #   ssh = {
-              #     icon = "folder-remote-symbolic";
-              #     placeholder = "";
-              #     prefix = "ssh ";
-              #     show_icon_when_single = true;
-              #     switcher_only = false;
-              #   };
-
-              #   switcher = {
-              #     icon = "application-default-icon";
-              #     prefix = "/";
-              #     show_icon_when_single = true;
-              #   };
-
-              #   symbols = {
-              #     placeholder = "";
-              #     prefix = "sym ";
-              #     switcher_only = false;
-              #   };
-
-              #   translation = {
-              #     icon = "translator";
-              #     placeholder = "";
-              #     prefix = "tr ";
-              #     switcher_only = false;
-              #   };
-
-              #   websearch = {
-              #     placeholder = "system-search";
-              #     switcher_only = false;
-              #     entries = [{}];
-              #   };
-              # };
-
-              # https://github.com/abenz1267/walker/wiki/Plugins
-              # plugins = [
-              #   {
-              #     # TODO:
-              #     # Search engines by keyword prefix
-              #     name = "search";
-              #     placeholder = "";
-              #     show_icon_when_single = true;
-              #     switcher_only = false;
-
-              #     src = "${pkgs.writeShellApplication {
-              #       name = "search";
-              #       text = readFile ./search.sh;
-              #       runtimeInputs = with pkgs; [coreutils jq xdg-utils];
-              #     }}/bin/search '%TERM%'";
-              #   }
-              # ];
             };
-
-          # https://github.com/abenz1267/walker/wiki/Theming
-          # theme = {
-          #   name = "custom";
-
-          #   style = ''
-          #     #box {
-          #       border: ${toString config.custom.border}px #073642 solid;
-          #       font: larger ${config.stylix.fonts.sansSerif.name};
-          #     }
-
-          #     ${readFile ./style.css}
-          #   '';
-
-          #   # https://github.com/abenz1267/walker/blob/master/internal/config/layout.default.toml
-          #   layout.ui.window = let
-          #     w = 750;
-          #     h = 300;
-          #   in {
-          #     width = w;
-          #     height = h;
-
-          #     box = {
-          #       h_align = "fill";
-          #       width = -1;
-          #       height = -1;
-
-          #       scroll = {
-          #         h_align = "fill";
-          #         h_scrollbar_policy = "external";
-          #         v_scrollbar_policy = "external";
-
-          #         list = {
-          #           width = -1;
-          #           height = -1;
-          #           min_width = -1;
-          #           min_height = -1;
-          #           max_width = w;
-          #           max_height = h;
-
-          #           item = {
-          #             text = {
-          #               sub = {
-          #                 hide = true; # Subtext
-          #               };
-          #             };
-          #           };
-          #         };
-          #       };
-          #     };
-          #   };
-          # };
-
-          # HACK: Allow child processes to live, otherwise applications launched through service are killed on stop
-          # https://www.freedesktop.org/software/systemd/man/latest/systemd.kill.html#KillMode=
-          # systemd.user.services.walker = {
-          #   Service = {
-          #     KillMode = "process";
-          #   };
-          # };
-
-          # # HACK: Create theme files for module prompt icons
-          # #?? MODULE.theme = "icon-ICON"
-          # # https://github.com/abenz1267/walker/blob/bb584eab3b0cc48ebfbac1a5da019864d74781c4/nix/hm-module.nix#L86
-          # xdg.configFile = listToAttrs (flatten (forEach cfg.icons (
-          #   icon: [
-          #     {
-          #       name = "walker/themes/icon-${icon}.css";
-          #       value = {text = hm.services.walker.theme.style;};
-          #     }
-          #     {
-          #       name = "walker/themes/icon-${icon}.json";
-          #       value = {
-          #         text = generators.toJSON {} (recursiveUpdate hm.services.walker.theme.layout {
-          #           ui.window.box.search.prompt.icon = icon;
-          #         });
-          #       };
-          #     }
-          #   ]
-          # )));
         };
       }
     ];
