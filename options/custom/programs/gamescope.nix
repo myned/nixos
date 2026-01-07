67a408d14e8335330f1b5933c0e64a2c6bb3cde8
Myned <dev@bjork.tech>
2026-01-07 15:19:08 -0600

    gamescope: fix args
    Signed-off-by: Myned <dev@bjork.tech>

--- options/custom/programs/gamescope.nix
+++ options/custom/programs/gamescope.nix
@@ -10,23 +10,16 @@ in {
     enable = mkEnableOption "gamescope";
 
     width = mkOption {
-      default = 1280;
+      default = config.custom.display.default.height * 16 / 9;
       description = "Width of the gamescope output display";
       example = 1920;
       type = types.int;
     };
 
     height = mkOption {
-      default = 800;
+      default = config.custom.display.default.height;
       description = "Height of the gamescope output display";
       example = 1080;
-      type = types.int;
-    };
-
-    refresh = mkOption {
-      default = 60;
-      description = "Refresh rate of the gamescope nested window";
-      example = 120;
       type = types.int;
     };
   };
@@ -40,9 +33,12 @@ in {
       capSysNice = true; # Allow renice
 
       args = [
+        "--rt"
+        "--force-grab-cursor"
+        "--fullscreen"
+        "--backend=sdl" # auto|drm|sdl|openvr|wayland
         "--output-width=${toString cfg.width}"
         "--output-height=${toString cfg.height}"
-        "--nested-refresh=${toString cfg.refresh}"
       ];
     };
   };
