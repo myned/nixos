e18e51060c9791a3a7591af433a2f01bc16e3783
Myned <dev@bjork.tech>
2026-02-28 22:04:51 -0600

    containers: add uptimekuma
    Signed-off-by: Myned <dev@bjork.tech>

---
+++ options/custom/containers/uptimekuma.nix
@@ -0,0 +1,38 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib; let
+  cfg = config.custom.containers.uptimekuma;
+  containerCfg = config.containers.uptimekuma;
+  hostCfg = config;
+in {
+  options.custom.containers.uptimekuma = {
+    enable = mkEnableOption "uptimekuma";
+  };
+
+  config = mkIf cfg.enable {
+    containers.uptimekuma = {
+      config = {
+        services.uptime-kuma = {
+          enable = true;
+          package = pkgs.unstable.uptime-kuma;
+
+          # https://github.com/louislam/uptime-kuma/wiki/Environment-Variables
+          settings = {
+            HOST = "::";
+            PORT = "80";
+          };
+        };
+
+        systemd.services.uptime-kuma.serviceConfig = {
+          PrivateUsers = mkForce false; # To use granted capabilities
+          AmbientCapabilities = mkForce ["CAP_NET_BIND_SERVICE"]; # For binding to ports < 1024
+          CapabilityBoundingSet = mkForce ["CAP_NET_BIND_SERVICE"];
+        };
+      };
+    };
+  };
+}
