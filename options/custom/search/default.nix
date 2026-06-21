208391e58042c680ef06f8a166df71415e961756
Myned <dev@bjork.tech>
2026-06-21 15:17:13 -0500

    search: use let binding for default option type
    Signed-off-by: Myned <dev@bjork.tech>

--- options/custom/search/default.nix
+++ options/custom/search/default.nix
@@ -1,20 +1,82 @@
 {
   config,
   lib,
-  options,
   ...
 }:
 with lib; let
   cfg = config.custom.search;
 in {
-  options.custom.search = {
+  options.custom.search = let
+    engineSubmodule = with types;
+      submodule ({name, ...}: {
+        options = {
+          name = mkOption {
+            description = "Name of the search engine";
+            default = name;
+            example = "google";
+            type = str;
+          };
+
+          title = mkOption {
+            description = "Title of the search engine";
+            default = "";
+            example = "Google";
+            type = str;
+          };
+
+          shortcut = mkOption {
+            description = "Shortcut keyword of the search engine";
+            default = "";
+            example = "g";
+            type = str;
+          };
+
+          iconUrl = mkOption {
+            description = "Icon URL of the search engine";
+            default = "";
+            example = "https://www.gstatic.com/images/branding/searchlogo/ico/favicon.ico";
+            type = str;
+          };
+
+          searchUrl = mkOption {
+            description = "Search URL of the search engine";
+            default = "";
+            example = "https://www.google.com/search?q=%s";
+            type = str;
+          };
+
+          suggestUrl = mkOption {
+            description = "Suggest URL of the search engine";
+            default = "";
+            example = "https://www.google.com/complete/search?q=%s";
+            type = str;
+          };
+        };
+      });
+  in {
     enable = mkEnableOption "search";
 
     default = mkOption {
-      description = "Default search engine";
+      description = "Submodule of the default search engine";
       default = cfg.engines.kagi;
       example = cfg.engines.google;
-      type = options.custom.search.engines.type.nestedTypes.elemType;
+      type = engineSubmodule;
+    };
+
+    engines = mkOption {
+      description = "Submodules of search engines";
+
+      example = {
+        google = {
+          title = "Google";
+          shortcut = "g";
+          iconUrl = "https://www.gstatic.com/images/branding/searchlogo/ico/favicon.ico";
+          searchUrl = "https://google.com/search?q=%s";
+          suggestUrl = "https://www.google.com/complete/search?q=%s";
+        };
+      };
+
+      type = types.attrsOf engineSubmodule;
     };
   };
 }
