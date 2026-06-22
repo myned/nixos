f634e60251f0e25e7bc3b9ede9d58634533fb319
Myned <dev@bjork.tech>
2026-06-21 20:44:54 -0500

    laptop: switch to ppd
    Signed-off-by: Myned <dev@bjork.tech>

--- profiles/laptop/default.nix
+++ profiles/laptop/default.nix
@@ -4,26 +4,14 @@
     profile = "laptop";
     desktop = "niri";
 
-    arion = {
-      enable = true;
-      user = config.custom.username;
-    };
-
     services = {
       #// auto-cpufreq.enable = true;
-      colord.enable = true;
+      #// colord.enable = true;
       ollama.download = ["potato" "medium"];
-      tuned.enable = true;
+      power-profiles-daemon.enable = true;
+      power-profiles-daemon.auto = true;
+      #// tuned.enable = true;
       #// wluma.enable = true;
-
-      # power-profiles-daemon = {
-      #   enable = true;
-      #   auto = true;
-      # };
-    };
-
-    settings = {
-      #// system76.enable = true;
     };
 
     vms = {
